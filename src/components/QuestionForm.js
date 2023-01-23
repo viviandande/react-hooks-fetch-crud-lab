ccc8ad0f1303de503b11d6281a62290ff22cb813
+ <+>
2023-01-23 11:44:25 +0300

    Completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,8 +19,24 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
-  }
+
+    fetch("http://localhost:3000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    cbody: JSON.stringify({
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    }),
+  });
+}
 
   return (
     <section>
